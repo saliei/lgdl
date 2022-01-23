37cc1783dc8faedb03716d4b47f52a01da37fa86
Saeid Aliei <saeidaliei2019@gmail.com>
2022-01-23 17:50:05 +0100

    resolve default file name

--- download.py
+++ download.py
@@ -5,12 +5,15 @@ from tqdm import tqdm
 
 from read_conf import get_proxies
 
+proxies = get_proxies()
+
 def download(url):
-    proxies = get_proxies()
     buffer_size = 1024
     response = requests.get(url, stream=True, proxies=proxies)
     file_size = int(response.headers.get("Content-Length", 0))
-    default_filename = title + '.' + extension
+    #TODO: get the name from query result
+    # default_filename = title + '.' + extension
+    default_filename = "test"
     content_disposition = response.headers.get("Content-Disposition")
     if content_disposition:
         value, params = cgi.parse_header(content_disposition)
@@ -18,7 +21,7 @@ def download(url):
     else:
         filename = default_filename
     
-    progress = tqdm(response.iter_content(buffer_size), f"Downloading {title}", \
+    progress = tqdm(response.iter_content(buffer_size), f"Downloading {filename}", \
             total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
     with open(filename, "wb") as f:
         for data in progress.iterable:
