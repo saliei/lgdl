9425d9701a650ce46e7f1d36f773cb781ae0d0d8
Saeid Aliei <saeidaliei2019@gmail.com>
2022-01-23 19:21:28 +0100

    add argument parser

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-import libgen_parser as parser
+#!/usr/bin/env python3
+import argparse
+
+import libgen_parser
 from display import pretty_print
 from download import download
 
-search_query = "quantum"
 mirrors_url = "https://sci-hub.41610.org/library-genesis"
 
-mirrors = parser.get_mirrors(mirrors_url)
-query_results = parser.parse_query(search_query)
-download_link = parser.get_download_url(query_results)
+parser = argparse.ArgumentParser(prog="libgen", \
+        description="Command Line Interface for Library Genesis.")
+parser.add_argument("search", type=str, help="general search query")
+parser.add_argument("-s", "--search", default=None, help="general search query")
+args = parser.parse_args()
+
+
+mirrors = libgen_parser.get_mirrors(mirrors_url)
+query_results = libgen_parser.parse_query(args.search)
+download_link = libgen_parser.get_download_url(query_results)
 
 for index, result in enumerate(query_results):
     pretty_print(result, index)
 
-download(download_link)
+# download(download_link)
 
