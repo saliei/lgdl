ee29e448a9eaec9fd6207f09de323da31af94c20
Saeid Aliei <saeidaliei2019@gmail.com>
2022-01-24 09:25:00 +0100

    add arguments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import sys
 
 import libgen_parser
 from display import pretty_print
@@ -7,19 +8,34 @@ from download import download
 
 mirrors_url = "https://sci-hub.41610.org/library-genesis"
 
-parser = argparse.ArgumentParser(prog="libgen", \
+parser = argparse.ArgumentParser(prog="lgdl", \
         description="Command Line Interface for Library Genesis.")
-parser.add_argument("search", type=str, help="general search query")
-parser.add_argument("-s", "--search", default=None, help="general search query")
+parser.add_argument("-s", "--search", nargs='?', type=str, help="search query")
 args = parser.parse_args()
-
 
 mirrors = libgen_parser.get_mirrors(mirrors_url)
 query_results = libgen_parser.parse_query(args.search)
-download_link = libgen_parser.get_download_url(query_results)
 
 for index, result in enumerate(query_results):
     pretty_print(result, index)
 
-# download(download_link)
+ids = sys.stdin.readline()
+ids = ids.rstrip().replace(' ', '').split(',')
 
+indices = []
+for _id in ids:
+    try:
+        _id = int(_id)
+        indices.append(_id)
+    except Exception as err:
+        if '-' in _id:
+            _id = [int(_id) for _id in _id.split('-')]
+            indices.append(_id)
+        else:
+            print("Indices Not accepted!")
+            raise
+
+download_links = libgen_parser.get_download_url(query_results, indices)
+for link in download_links:
+    download(link)
+            
