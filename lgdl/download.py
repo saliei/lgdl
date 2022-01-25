f8e83cd4f1dc34d8aa8d684c25d86246e5e85575
Saeid Aliei <saeidaliei2019@gmail.com>
2022-01-25 18:38:47 +0100

    add title as a variable

--- lgdl/download.py
+++ lgdl/download.py
@@ -3,13 +3,12 @@ import sys
 import cgi
 from tqdm import tqdm
 
-from read_conf import get_proxies
+from read_config import parse_config_file
+config = parse_config_file()
 
-proxies = get_proxies()
-
-def download(url):
+def download(url, title):
     buffer_size = 1024
-    response = requests.get(url, stream=True, proxies=proxies)
+    response = requests.get(url, stream=True, proxies=config["proxies"])
     file_size = int(response.headers.get("Content-Length", 0))
     #TODO: get the name from query result
     # default_filename = title + '.' + extension
@@ -21,7 +20,7 @@ def download(url):
     else:
         filename = default_filename
     
-    progress = tqdm(response.iter_content(buffer_size), f"Downloading {filename}", \
+    progress = tqdm(response.iter_content(buffer_size), f"Downloading {title}", \
             total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
     with open(filename, "wb") as f:
         for data in progress.iterable:
