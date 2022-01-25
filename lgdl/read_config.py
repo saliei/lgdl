73aa871df1f2300ee757060d9e7e2d3f04e7245b
Saeid Aliei <saeidaliei2019@gmail.com>
2022-01-25 18:39:21 +0100

    read resolution from config file

--- lgdl/read_config.py
+++ lgdl/read_config.py
@@ -1,7 +1,7 @@
 import re
 import ast
 
-config_filename = "lgdl.conf"
+config_filename = "../lgdl.conf"
 
 def read_conf_file(filename):
     with open(filename, "r") as conf_file:
@@ -14,22 +14,41 @@ def strip_comments(conf_content):
     return content_without_comment
 
 def get_substring(string, start, end):
-    l1_proxies = string.find(start)
-    l2_proxies = string.find(end)
-    substring  = string[l1_proxies:l2_proxies+1]
+    l1 = string.find(start)
+    l2 = string.find(end)
+    substring  = string[l1:l2+1]
     return substring
 
-def get_proxies():
-    conf_content = read_conf_file(config_filename)
-    conf_content = strip_comments(conf_content)
-    proxies_substring = get_substring(conf_content, "proxies", "}")
+def get_proxies(config):
+    proxies_substring = get_substring(config, "proxies", "}")
     proxies_substring = proxies_substring.replace("\n", '')
-    proxies_substring = proxies_substring.replace(" " , '')
     if proxies_substring:
-        proxies = ast.literal_eval(proxies_substring.partition('=')[2])
+        proxies = ast.literal_eval(proxies_substring.partition('=')[-1])
     else:
         # tor socks proxy
         proxies = { 'http' : 'socks5h://127.0.0.1:9050',
                     'https': 'socks5h://127.0.0.1:9050' }
     return proxies
+
+def get_resolution(config):
+    conf_lines = config.split("\n")
+    for line in conf_lines:
+        if line.startswith("resolution"):
+            resolution = int(line.partition('=')[-1])
+    if resolution not in [25, 50, 75, 100]:
+        print("Warning: accepted resolutions are 25, 50, 75, or 100.\
+                Got: {}. Setting resolution to 25.".format(resolution))
+        resolution = 25
+
+    return resolution
+
+def parse_config_file():
+    conf_content = read_conf_file(config_filename)
+    conf_content = strip_comments(conf_content)
+    conf_content = conf_content.replace(' ', '')
+    proxies = get_proxies(conf_content)
+    resolution = get_resolution(conf_content)
+    config = {"proxies": proxies, "resolution": resolution}
+
+    return config
 
